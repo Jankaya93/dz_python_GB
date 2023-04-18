c3160a204c400388284fafc4453176c3a419bf8c
Oreshek <122908228+Jankaya93@users.noreply.github.com>
2023-04-18 19:57:46 +0300

    Apply suggestions from code review
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- dz_3/dz_3_3.py
+++ dz_3/dz_3_3.py
@@ -17,13 +17,13 @@ K – 5 очков; J, X – 8 очков; Q, Z – 10 очков.
 ноутбук
 12
 """
-liter_1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
-liter_2 = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
-liter_3 = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
-liter_4 = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
-liter_5 = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
-liter_8 = ['J', 'X', 'Ш', 'Э', 'Ю']
-liter_10 = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+liter_1 = 'AEIOULNSTRАВЕИНОРСТ'
+liter_2 = 'DGДКЛМПУ'
+liter_3 = 'BCMPБГЁЬЯ'
+liter_4 = 'FHVWYЙЫ'
+liter_5 = 'KЖЗХЦЧ'
+liter_8 = 'JXШЭЮ'
+liter_10 = 'QZФЩЪ'
 
 dictionary = dict.fromkeys(liter_1, 1) | dict.fromkeys(liter_2, 2) | dict.fromkeys(liter_3, 3) | dict.fromkeys(
     liter_4, 4) | dict.fromkeys(liter_5, 5) | dict.fromkeys(liter_8, 8) | dict.fromkeys(liter_10, 10)
